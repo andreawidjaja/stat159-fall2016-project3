ea77d04e552001320dbee4cb8fcb981a16215edb
Priscilla Hartono <phartono@berkeley.edu>
2016-11-14 20:04:08 -0800

    code/scripts: started eda.R

---
+++ code/scripts/eda.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+#Read 2010-2014 data set
+2010 <- read.csv('../../data/CW2010.csv')
+2011 <- read.csv('../../data/CW2011.csv')
+2012 <- read.csv('../../data/CW2012.csv')
+2013 <- read.csv('../../data/CW2013.csv')
+2014 <- read.csv('../../data/CW2014.csv')
+
