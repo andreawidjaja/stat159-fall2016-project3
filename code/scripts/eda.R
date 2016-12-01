12b78ae02667b8c5f625e7a48f55b3f18813b2fe
Andrea Widjaja <andrea.widjaja@gmail.com>
2016-11-30 18:59:18 -0800

    code, data, images: finished up qualitative variables

--- code/scripts/eda.R
+++ code/scripts/eda.R
@@ -14,7 +14,7 @@ selected_data <- data %>%
 #Quantitative variables
 as.integer(clean_data$ADM_RATE)
 quantitative <- clean_data %>%
-  select("ADM_RATE", "SATVR25", "SATVR75", "SATMT25", "SATMT75", "SATWR25", "SATWR75",
+  dplyr::select("ADM_RATE", "SATVR25", "SATVR75", "SATMT25", "SATMT75", "SATWR25", "SATWR75",
          "SATVRMID", "SATMTMID", "SATWRMID", "ACTCM25", "ACTCM75", "ACTEN25", "ACTEN75", "ACTMT25", "ACTMT75",
          "ACTWR25", "ACTWR75", "ACTCMMID", "ACTENMID", "ACTMTMID", "ACTWRMID", "SAT_AVG", "PCIP01", "PCIP03",
          "PCIP04", "PCIP05", "PCIP09", "PCIP10", "PCIP11", "PCIP12", "PCIP13", "PCIP14", "PCIP15", "PCIP16",
@@ -27,11 +27,128 @@ quantitative <- clean_data %>%
          "UGDS_BLACK", "UGDS_HISP", "UGDS_ASIAN", "UGDS_AIAN", "UGDS_NHPI", "UGDS_2MOR", "UGDS_NRA", "UGDS_UNKN",
          "PPTUG_EF", "NPT4_048_PUB", "NPT4_3075_PUB", "NPT4_75UP_PUB")
 
+#ADM_RATE: Admission rate
+#SATVR25: 25th percentile of SAT scores at the institution (critical reading)
+#SATVR75: 75th percentile of SAT scores at the institution (critical reading)
+#SATMT25: 25th percentile of SAT scores at the institution (math)
+#SATMT75: 75th percentile of SAT scores at the institution (math)
+#SATWR25: 25th percentile of SAT scores at the institution (writing)
+#SATWR75: 75th percentile of SAT scores at the institution (writing)
+#SATVRMID: Midpoint of SAT scores at the institution (critical reading)
+#SATMTMID: Midpoint of SAT scores at the institution (math)
+#SATWRMID: Midpoint of SAT scores at the institution (writing)
+#ACTCM25: 25th percentile of the ACT cumulative score
+#ACTCM75: 75th percentile of the ACT cumulative score
+#ACTEN25: 25th percentile of the ACT English score
+#ACTEN75: 75th percentile of the ACT English score
+#ACTMT25: 25th percentile of the ACT math score
+#ACTMT75: 75th percentile of the ACT math score
+#ACTWR25: 25th percentile of the ACT writing score
+#ACTWR75: 75th percentile of the ACT writing score
+#ACTCMMID: Midpoint of the ACT cumulative score
+#ACTENMID: Midpoint of the ACT English score
+#ACTMTMID: Midpoint of the ACT math score
+#ACTWRMID: Midpoint of the ACT writing score
+#SAT_AVG: Average SAT equivalent score of students admitted
+#PCIP01: Percentage of degrees awarded in Agriculture, Agriculture Operations, And Related Sciences.
+#PCIP03: Percentage of degrees awarded in Natural Resources And Conservation.  
+#PCIP04: Percentage of degrees awarded in Architecture And Related Services.
+#PCIP05: Percentage of degrees awarded in Area, Ethnic, Cultural, Gender, And Group Studies.
+#PCIP09: Percentage of degrees awarded in Communication, Journalism, And Related Programs.
+#PCIP10: Percentage of degrees awarded in Communications Technologies/Technicians And Support Services.
+#PCIP11: Percentage of degrees awarded in Computer And Information Sciences And Support Services.
+#PCIP12:Percentage of degrees awarded in Personal And Culinary Services.
+#PCIP13:Percentage of degrees awarded in Education.
+#PCIP14:Percentage of degrees awarded in Engineering.
+#PCIP15: Percentage of degrees awarded in Engineering Technologies And Engineering-Related Fields.
+#PCIP16: Percentage of degrees awarded in Foreign Languages, Literatures, And Linguistics.
+#PCIP19: Percentage of degrees awarded in Family And Consumer Sciences/Human Sciences.
+#PCIP22: Percentage of degrees awarded in Legal Professions And Studies.
+#PCIP23: Percentage of degrees awarded in English Language And Literature/Letters.
+#PCIP24: Percentage of degrees awarded in Liberal Arts And Sciences, General Studies And Humanities.
+#PCIP25: Percentage of degrees awarded in Library Science.
+#PCIP26: Percentage of degrees awarded in Biological And Biomedical Sciences.
+#PCIP27: Percentage of degrees awarded in Mathematics And Statistics.
+#PCIP29: Percentage of degrees awarded in Military Technologies And Applied Sciences.
+#PCIP30: Percentage of degrees awarded in Multi/Interdisciplinary Studies.
+#PCIP31: Percentage of degrees awarded in Parks, Recreation, Leisure, And Fitness Studies.
+#PCIP38: Percentage of degrees awarded in Philosophy And Religious Studies.
+#PCIP39: Percentage of degrees awarded in Theology And Religious Vocations.
+#PCIP40: Percentage of degrees awarded in Physical Sciences.
+#PCIP41: Percentage of degrees awarded in Science Technologies/Technicians.
+#PCIP42: Percentage of degrees awarded in Psychology.
+#PCIP43: Percentage of degrees awarded in Homeland Security, Law Enforcement, Firefighting And Related Protective Services.
+#PCIP44: Percentage of degrees awarded in Public Administration And Social Service Professions.
+#PCIP45: Percentage of degrees awarded in Social Sciences.
+#PCIP46: Percentage of degrees awarded in Construction Trades.
+#PCIP47: Percentage of degrees awarded in Mechanic And Repair Technologies/Technicians.
+#PCIP48: Percentage of degrees awarded in Precision Production.
+#PCIP49: Percentage of degrees awarded in Transportation And Materials Moving.
+#PCIP50: Percentage of degrees awarded in Visual And Performing Arts.
+#PCIP51: Percentage of degrees awarded in Health Professions And Related Programs.
+#PCIP52: Percentage of degrees awarded in Business, Management, Marketing, And Related Support Services.
+#PCIP54: Percentage of degrees awarded in History.
+#NPT4_PUB: Average net price for Title IV institutions (public institutions)
+#NPT41_PUB: Average net price for $0-$30,000 family income (public institutions)
+#NPT42_PUB: Average net price for $30,001-$48,000 family income (public institutions)
+#NPT43_PUB: Average net price for $48,001-$75,000 family income (public institutions)
+#NPT44_PUB: Average net price for $75,001-$110,000 family income (public institutions)
+#NPT45_PUB: Average net price for $110,000+ family income (public institutions)
+#NUM4_PUB: Number of Title IV students (public institutions)
+#NUM41_PUB: Number of Title IV students, $0-$30,000 family income (public institutions)
+#NUM42_PUB: Number of Title IV students, $30,001-$48,000 family income (public institutions)
+#NUM43_PUB: Number of Title IV students, $48,001-$75,000 family income (public institutions)
+#NUM44_PUB: Number of Title IV students, $75,001-$110,000 family income (public institutions)
+#NUM45_PUB: Number of Title IV students, $110,000+ family income (public institutions)
+#COSTT4_A: Average cost of attendance (academic year institutions)
+#TUITIONFEE_IN: In-state tuition and fees
+#TUITIONFEE_OUT: Out-of-state tuition and fees
+#AVGFACSAL: Average faculty salary
+#PFTFAC: Proportion of faculty that is full-time
+#AGE_ENTRY: Average age of entry, via SSA data
+#FEMALE: Share of female students, via SSA data
+#MARRIED: Share of married students
+#DEPENDENT: Share of dependent students
+#VETERAN: Share of veteran students
+#FIRST_GEN: Share of first-generation students
+#C150_4: Completion rate for first-time, full-time students at four-year institutions (150% of expected time to completion/6 years)
+#UGDS: Enrollment of undergraduate certificate/degree-seeking students
+#UGDS_WHITE: Total share of enrollment of undergraduate degree-seeking students who are white
+#UGDS_BLACK: Total share of enrollment of undergraduate degree-seeking students who are black
+#UGDS_HISP: Total share of enrollment of undergraduate degree-seeking students who are Hispanic
+#UGDS_ASIAN: Total share of enrollment of undergraduate degree-seeking students who are Asian
+#UGDS_AIAN: Total share of enrollment of undergraduate degree-seeking students who are American Indian/Alaska Native
+#UGDS_NHPI: Total share of enrollment of undergraduate degree-seeking students who are Native Hawaiian/Pacific Islander
+#UGDS_2MOR: Total share of enrollment of undergraduate degree-seeking students who are two or more races
+#UGDS_NRA: Total share of enrollment of undergraduate degree-seeking students who are non-resident aliens
+#UGDS_UNKN: Total share of enrollment of undergraduate degree-seeking students whose race is unknown
+#PPTUG_EF: Share of undergraduate, degree-/certificate-seeking students who are part-time
+#NPT4_048_PUB: Average net price for $0-$48,000 family income (public institutions)
+#NPT4_3075_PUB: Average net price for $30,001-$75,000 family income (public institutions)
+#NPT4_75UP_PUB: Average net price for $75,000+ family income (public institutions)
+
+
+
+
 #Qualitative variables
 qualitative <- selected_data %>%
   select(REGION)
 
+#REGION
+#0	U.S. Service Schools
+#1	New England (CT, ME, MA, NH, RI, VT)
+#2	Mid East (DE, DC, MD, NJ, NY, PA)
+#3	Great Lakes (IL, IN, MI, OH, WI)
+#4	Plains (IA, KS, MN, MO, NE, ND, SD)
+#5	Southeast (AL, AR, FL, GA, KY, LA, MS, NC, SC, TN, VA, WV)
+#6	Southwest (AZ, NM, OK, TX)
+#7	Rocky Mountains (CO, ID, MT, UT, WY)
+#8	Far West (AK, CA, HI, NV, OR, WA)
+#9	Outlying Areas (AS, FM, GU, MH, MP, PR, PW, VI)
+#===========================================================================================================================
+
 #Summary statistics of quantitative variables
+
 #min, 1st quartile, median, mean, 3rd quartile, max
 quantitative_summary <- summary(quantitative)
 #range
@@ -40,15 +157,20 @@ quantitative_range <- apply(quantitative, 2, FUN=range)
 quantitative_IQR <- apply(quantitative, 2, FUN=IQR)
 #standard deviation
 quantitative_SD <- apply(quantitative, 2, FUN=sd)
-#histograms
+
+#---------------------------------------------------------------------------------------------------------------------------
+#Histograms
+
 #histogram of average ACT scores
 png("../../images/histogram_ACT_avg.png")
 hist(clean_data$ACTCMMID, main = "Histogram of Average of ACT Scores")
 dev.off()
+
 #histogram of average SAT scores
 png("../../images/histogram_SAT_avg.png")
 hist(clean_data$SAT_AVG, main = "Histogram of Average of SAT Scores")
 dev.off()
+
 #histogram of percentage of degrees of different majors
 pairs_majors = clean_data[, c("PCIP01", "PCIP03", "PCIP04", "PCIP05", "PCIP09", "PCIP10",
                               "PCIP11", "PCIP12", "PCIP13", "PCIP14", "PCIP15", "PCIP16",
@@ -100,6 +222,7 @@ hist(PCIP52, main = "Histogram of % of Degrees in Business, Management, Marketin
 hist(PCIP54, main = "Histogram of % of Degrees in History")
 par(graph_majors)
 dev.off()
+
 #histogram of enrollment by race
 pairs_race_enrollment = clean_data[, c("UGDS_WHITE", "UGDS_BLACK", "UGDS_HISP", "UGDS_ASIAN", "UGDS_AIAN", "UGDS_NHPI")]
 png("../../images/histogram_race_enrollment.png")
@@ -113,14 +236,17 @@ hist(UGDS_AIAN, main = "Histogram of Share of Enrollment who are American Indian
 hist(UGDS_NHPI, main = "Histogram of Share of Enrollment who are Hawaiian/Pacific Islander")
 par(graph_race_enrollment)
 dev.off()
+
 #histogram of average cost per academic year
 png("../../images/histogram_net_price.png")
 hist(clean_data$COSTT4_A, main = "Histogram of Average Cost per Academic Year")
 dev.off()
+
 #histogram of completion rate
 png("../../images/histogram_completion.png")
 hist(clean_data$C150_4, main = "Histogram of Completion Rate")
 dev.off()
+
 #histogram of completion rate by income level
 pairs_income_completion = clean_data[, c("COMP_ORIG_YR6_RT", "LO_INC_COMP_ORIG_YR6_RT", "LO_INC_COMP_4YR_TRANS_YR6_RT", "MD_INC_COMP_ORIG_YR6_RT",
                                       "HI_INC_COMP_ORIG_YR6_RT")]
@@ -148,15 +274,33 @@ hist(C150_4_NHPI, main = "Histogram of Completion Rate in 6 years who are Hawaii
 par(graph_race_completion)
 dev.off()
 
+#---------------------------------------------------------------------------------------------------------------------------
 #Summary statistics of qualitative variables
-png("../../images/histogram_region.png")
-hist(selected_data$REGION)
-dev.off()
+summary_qual <- summary(qualitative)
 
+#Table of Relative Frequency of Qualitative Variables
+#without Proportion
+freq <- ftable(table(qualitative))
+#With proportion
+prop <- prop.table(ftable(table(qualitative)))
+#---------------------------------------------------------------------------------------------------------------------------
+#Barchart for Region qualitative variable
+png("../../images/barchart_region.png")
+barplot(table(qualitative$REGION), main="Barchart of Region")
+dev.off
+
+#Conditional Boxplots for Region qualitative vairables
+png("../../images/conditional_boxplot_gender.png")
+boxplot(C150_4 ~ REGION, data=selected_data, main="Conditional Boxplot of Region")
+dev.off
+#===========================================================================================================================
 #Correlation Matrix
 matrix <- cor(clean_data)
 save(matrix, file = "../../data/correlation_matrix.RData")
 
+#Anova's between Completion rate and all qualitative variables
+anova <- aov(C150_4~REGION, selected_data)
+#===========================================================================================================================
 #Generate eda.txt for quantitative variables
 sink("../../data/eda.txt")
 #Summary statistics of quantitative variables
@@ -177,6 +321,33 @@ cat("\n\n")
 #standard deviation
 cat("Standard Deviation of Quantitative Variables\n")
 print(quantitative_SD)
+cat("\n\n")
+#Summary statistics of Qualitative variables
+cat("Summary Statistics of Qualitative Variables\n")
+cat("\n\n")
+#Table of frequency
+cat("Table of Frequency of qualitative variables\n")
+print(summary_qual)
+cat("\n\n")
+#Table of relative frequency without proportion
+cat("Table of Relative Frequency of qualitative variables\n\n")
+cat("without Proportion\n\n")
+print(freq)
+cat("\n\n")
+#Table of relative frequency with proportion
+cat("with Proportion\n\n")
+print(prop)
+cat("\n\n")
+
+cat("Additional data\n")
+cat("\n\n")
+#Matrix of Correlation
+cat("Matrix of Correlation\n")
+print(correlation_matrix)
+cat("\n\n")
+#Anova between Completion Rate and qualitative varible
+cat("Anova's between Completion Rate and qualitative variables\n")
+print(anova)
 cat("\n\n")
 sink()
 
